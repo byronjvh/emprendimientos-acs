b442a2f96355b46ac7cb5d1faa4203a01bebd1af
Byron Vargas Hernandez <byronjvh02@gmail.com>
2026-05-08 13:29:09 -0600

    first commit

---
+++ src/emprendimientos.js
@@ -0,0 +1,51 @@
+import pijamasDass from "./assets/pijamas-dass/PIJAMAS_DASS.png";
+import pijamasDassLogo from "./assets/pijamas-dass/PIJAMAS_DASS_logo.png";
+import deliciasTita from "./assets/delicias-mi-tita/Delicias_mi_tita.jpeg";
+import deliciasTitaLogo from "./assets/delicias-mi-tita/delicias-mi-tita-logo.webp";
+import blossomDelicat from "./assets/blossom-delicat/blossom-delicat.png";
+import blossomDelicatLogo from "./assets/blossom-delicat/blossom-delicat-logo.png";
+
+export const emprendimientos = [
+    {
+        "title": "Pijamas Dass",
+        "image": pijamasDass,
+        "logo": pijamasDassLogo,
+        "description": "Pijamas Dass es el emprendimiento de mi madre, profesional graduada del INA, dedicada a la confección, ajustes de uniformes y sublimación personalizada con acabados de alta calidad.",
+        "category": "👗 Moda",
+        "name": "Joselinne Vargas",
+        "position": "Asesora Call Center",
+        "whatsapp": "https://wa.me/50683636171",
+        "facebook": "",
+        "instagram": "",
+        "website": "",
+        "linkText": "8363-6171"
+    },
+    {
+        "title": "Delicias Mi Tita",
+        "image": deliciasTita,
+        "logo": deliciasTitaLogo,
+        "description": "Delicias Mi Tita ofrece productos artesanales como miel, café, mermeladas, aceites saborizados, pan casero y repostería, elaborados con sabor tradicional y dedicación.",
+        "category": "🍰 Gastronomía",
+        "name": "Nicole Carrazco",
+        "position": "Asesora Call Center",
+        "whatsapp": "",
+        "facebook": "https://www.facebook.com/share/1B7AdZzrSy/",
+        "instagram": "",
+        "website": "",
+        "linkText": "Ver página"
+    },
+    {
+        "title": "Blossom Delicat",
+        "image": blossomDelicat,
+        "logo": blossomDelicatLogo,
+        "description": "En Blossom Delicat no solo vendemos, también te asesoramos para que encuentres lo ideal según tu estilo y necesidades.",
+        "category": "💅 Belleza",
+        "name": "Abigail Valverde",
+        "position": "Comercial",
+        "whatsapp": "https://chat.whatsapp.com/BKSlGURnT0hEckSLotHARF",
+        "facebook": "https://www.facebook.com/share/1Cs3gRC3cH/",
+        "instagram": "https://www.instagram.com/blossom_delicat",
+        "website": "",
+        "linkText": ""
+    }
+]
